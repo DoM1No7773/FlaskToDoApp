0cae25faa72d86fc1a5a74a087f64dcb3b518c7e
Dominik Grzegolec <grzegolecdominik@gmail.com>
2022-11-28 13:42:36 +0100

    adding delete option

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-from flask import Flask
+from flask import Flask,request,redirect
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 
+db = SQLAlchemy(app)
 
-@app.route("/")
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50),nullable=False)
+    content = db.Column(db.String(200),nullable=False)
+
+@app.route("/",methods=['POST','GET'])
 def index():
-    return render_template('base.html')
+    if request.method == 'POST':
+        inputTitle = request.form['taskTitle']
+        inputContent = request.form['taskContent']
 
+        db.session.add(Todo(title=inputTitle,content=inputContent))
+        db.session.commit()
+        return redirect('/')
+    else:
+        todos = Todo.query.all()
+        return render_template('index.html',todos=todos)
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    db.session.delete(Todo.query.get_or_404(id))
+    db.session.commit()
+    return redirect('/')
+
+if __name__ == "__main__":
+    app.run(debug=True)
